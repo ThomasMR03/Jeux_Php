473740e36f21954c63909990de03cdb0c18bd8d9
Thomas <Thomas.Face03@gmail.com>
2017-03-07 14:00:29 +0100

    First push

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exo Php</title>
+</head>
+<body>
+
+<header>
+	<a href="variable.php">Exo 1</a>
+</header>
+
+<footer>
+	
+</footer>
+
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,15 @@
+<?php
+$nom ="Thomas";
+$prenom = "MILLIEN";
+$age = "21";
+
+echo "$nom, $prenom, $age";
+?>
+
+<?php
+$km = 1;
+echo " </br> $km";
+$km = 3;
+echo "</br> $km";
+$km = 125;
+echo "</br> $km";

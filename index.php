cd855fe1bff0c7d208e8840fef5193a50aa2f1ab
Thomas <Thomas.Face03@gmail.com>
2017-03-07 14:06:35 +0100

    Un peu de css

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
 <head>
 	<title>Exo Php</title>
 </head>
 <body>
 
 <header>
-	<a href="variable.php">Exo 1</a>
+	<nav id="nav">
+		<ul>
+			<li>
+				<a href="variable.php">Exo 1</a>
+			</li>
+		</ul>
+	</nav>
 </header>
 
 <footer>

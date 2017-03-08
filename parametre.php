344bc2b6b4133e639213815818114b1e80757f9f
Thomas <Thomas.Face03@gmail.com>
2017-03-08 15:34:27 +0100

    exo 4

--- parametre.php
+++ parametre.php
@@ -1,3 +1,4 @@
 <a href="parametre/exo1.php?nom=Nemare&prenom=Jean">Exo 1</a>
 <a href="parametre/exo2.php?nom=Nemare&prenom=Jean">Exo 2</a>
 <a href="parametre/exo3.php?dateDebut=2/05/2016&dateFin=27/11/2016">Exo 3</a>
+<a href="parametre/exo4.php?langage=PHP&serveur=LAMP">Exo 4</a>
---
+++ parametre/exo4.php
@@ -0,0 +1,6 @@
+<?php
+ $langage = $_GET['langage'];
+ $serveur = $_GET['serveur'];
+?>
+ <?php echo $langage;?>;
+ <?php echo $serveur;?>;

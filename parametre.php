7ff446a07ca01c4ad6e7b986f4ec049d37fcdd00
Thomas <Thomas.Face03@gmail.com>
2017-03-08 15:43:34 +0100

    exo 7

--- parametre.php
+++ parametre.php
@@ -4,3 +4,4 @@
 <a href="parametre/exo4.php?langage=PHP&serveur=LAMP">Exo 4</a>
 <a href="parametre/exo5.php?semaine=12">Exo 5</a>
 <a href="parametre/exo6.php?batiment=12&salle=101">Exo 6</a>
+<a href="parametre/exo7.php">Exo 7</a>

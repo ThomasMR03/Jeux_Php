c20aef9118e63f657758ce6db3588cd286afe9a4
Thomas <Thomas.Face03@gmail.com>
2017-03-08 15:39:13 +0100

    exo 6

--- parametre.php
+++ parametre.php
@@ -3,4 +3,4 @@
 <a href="parametre/exo3.php?dateDebut=2/05/2016&dateFin=27/11/2016">Exo 3</a>
 <a href="parametre/exo4.php?langage=PHP&serveur=LAMP">Exo 4</a>
 <a href="parametre/exo5.php?semaine=12">Exo 5</a>
-<a href="">Exo 6</a>
+<a href="parametre/exo6.php?batiment=12&salle=101">Exo 6</a>
---
+++ parametre/exo6.php
@@ -0,0 +1,6 @@
+<?php
+ $batiment = $_GET['batiment'];
+ $salle = $_GET['salle'];
+?>
+ <?php echo $batiment;?>;
+ <?php echo $salle;?>;

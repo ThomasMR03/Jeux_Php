c132603a93f38d72c8625ee24af338a2afb63d7a
Thomas <Thomas.Face03@gmail.com>
2017-03-08 14:05:07 +0100

    exo 7 + correction

--- tableau.php
+++ tableau.php
@@ -27,14 +27,27 @@
 
 <div>
 	<?php
-	$tableau= array( 
-[02]=> "Aisne" ,
-[59]=> "Nord" ,
-[60]=> "Oise" ,
-[62]=> "Pas-de-Calais" ,
-[80]=> "Somme"
+	$tableau = array( 
+02 => "Aisne",
+59 => "Nord",
+60 => "Oise",
+62 => "Pas-de-Calais",
+80 => "Somme"
 );
 ?>
+</div>
+
+<div>
+	<?php
+	var_dump($tableau[59]);
+	?>
+</div>
+
+<div>
+	<?php
+	$tableau[] = array(51 => "Reims");
+	print_r($tableau);
+	?>
 </div>
 
 </body>

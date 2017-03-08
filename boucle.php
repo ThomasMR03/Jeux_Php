718b2c3f69ea2159223fbb13f999d16c8d422524
Thomas <Thomas.Face03@gmail.com>
2017-03-08 09:41:34 +0100

    exo 1

---
+++ boucle.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Les boucles</title>
+</head>
+<body>
+
+<div>
+	<?php
+$var = 0;
+while ($var <= 10) {
+	echo "$var";
+	$var++;
+}
+?>
+</div>
+
+</body>
+</html>

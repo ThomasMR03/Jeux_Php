367778594d09b294851705d6fec70eee2b6df0db
Thomas <Thomas.Face03@gmail.com>
2017-03-08 11:09:21 +0100

    exo 1

---
+++ function.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Les fonctions</title>
+</head>
+<body>
+
+<div>
+	<?php
+	function true ($var, $var2){
+	if ($var == $var2)
+		return (TRUE);
+}
+	?>
+</div>
+
+</body>
+</html>

f9ee3175c27df76f963eb324e1be380ae59240dc
Thomas <Thomas.Face03@gmail.com>
2017-03-09 16:08:26 +0100

    exo 4

--- date.php
+++ date.php
@@ -5,4 +5,6 @@ $date = date("d-m-Y");
 print("</br>Nous sommes le $date");
 $date = date("l/j/F/Y");
 print("<br>Nous sommes le $date");
-echo strftime("</br>%A %e %B %Y");
+echo strftime("</br>%A %e %B %Y</br>");
+echo time().'</br>';
+echo mktime(15, 34, 12, 03, 2, 2016).'</br>';

22c4803e5475a84188d947de83cc46254c3a8666
Thomas <Thomas.Face03@gmail.com>
2017-03-09 15:55:46 +0100

    exo 3

--- date.php
+++ date.php
@@ -3,3 +3,6 @@ $date = date("d/m/Y");
 print("Nous sommes le $date");
 $date = date("d-m-Y");
 print("</br>Nous sommes le $date");
+$date = date("l/j/F/Y");
+print("<br>Nous sommes le $date");
+echo strftime("</br>%A %e %B %Y");

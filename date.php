2d85003ef4a24586aaad6fb47d6d16446a80df73
Thomas <Thomas.Face03@gmail.com>
2017-03-09 16:31:34 +0100

    exo 5

--- date.php
+++ date.php
@@ -8,3 +8,7 @@ print("<br>Nous sommes le $date");
 echo strftime("</br>%A %e %B %Y</br>");
 echo time().'</br>';
 echo mktime(15, 34, 12, 03, 2, 2016).'</br>';
+$datetime1 = new DateTime('2016-05-16');
+$datetime2 = new DateTime();
+$interval = $datetime1->diff($datetime2);
+echo $interval->format('%a jours').'</br>';
